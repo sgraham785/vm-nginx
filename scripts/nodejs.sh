72e5e9966f66d04c53e510890417deeb7e4f5704
Sean Graham <admin@example.com>
2015-09-24 19:05:05 -0700

    udpated nodejs build script and adjusted configs

--- scripts/nodejs.sh
+++ scripts/nodejs.sh
@@ -4,9 +4,30 @@ echo 'RUNNING NODEJS INSTALL....'
 
 curl --silent --location https://rpm.nodesource.com/setup | bash -
 
-echo 'Install Nodejs & NPM'
-yum -y install nodejs
-yum install gcc-c++ make
+# update gcc
+wget http://people.centos.org/tru/devtools-2/devtools-2.repo -O /etc/yum.repos.d/devtoolset-2.repo
+yum -y install devtoolset-2-gcc-4.8.2 devtoolset-2-gcc-c++-4.8.2 make
+/opt/rh/devtoolset-2/root/usr/bin/gcc --version
+rm -rf /usr/bin/gcc
+ln -s /opt/rh/devtoolset-2/root/usr/bin/gcc /usr/bin/gcc
+gcc --version
+
+rm -rf /usr/bin/g++
+ln -s /opt/rh/devtoolset-2/root/usr/bin/g++ /usr/bin/g++
+
+
+echo 'Install Node v4.1.1 & NPM'
+cd /usr/src
+wget http://nodejs.org/dist/v4.1.1/node-v4.1.1.tar.gz
+tar zxvf node-v4.1.1.tar.gz
+cd node-v4.1.1
+./configure
+make && make install
+
+mv /usr/local/bin/node /usr/bin/
+node -v
+
+ln -s ../local/lib/node_modules/npm/bin/npm-cli.js npm
 
 echo 'Update NPM...'
 npm install -g npm
@@ -14,6 +35,9 @@ npm -v
 
 echo 'Install Gulp'
 npm install gulp -g
+
+echo 'Install Nodemon'
+npm install -g nodemon
 
 echo 'Output versions'
 node --version
--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
+service postgresql-9.4 restart
+
 service nginx restart
+
 
 # /var/www/html/styleguides/node_modules/.bin/kss-node --config /var/www/html/styleguides/node_modules/kss/demo/kss-config.json
 # /var/www/html/styleguides/node_modules/.bin/kss-node --config /var/www/html/styleguides/portal/kss-config.json
